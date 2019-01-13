e85016b4397d23d82e28a4734b6cc6e56e0d25f5
bob kerner <bobkerner@bobs-Mac-Pro.local>
2019-01-13 17:02:43 -0500

    updated to python 3.x

--- example.py
+++ example.py
@@ -1,26 +1,25 @@
 import numpy as np
+ 
 from trackbar import *
 
-class MyTest(unittest.TestCase):
-    def test_parameter_tuner(self):
-        img = np.zeros((480,640,3))
-        midpoint = tuple(np.roll(np.array(img.shape[0:2])/2,1))
+ 
+img = np.zeros((480,640,3))
+midpoint = tuple(np.roll(np.array(img.shape[0:2])/2,1).astype(dtype=np.int))
 
-        def draw_image(point):
-            new_image = np.copy(img)
-            (x,y) = point
-            cv2.line(new_image,(midpoint[0]+x,midpoint[1]+y),midpoint,[255,255,255])
-            return new_image
+def draw_image(point):
+    new_image = np.copy(img)
+    (x,y) = point
+    cv2.line(new_image,(midpoint[0]+x,midpoint[1]+y),midpoint,[255,255,255])
+    return new_image
 
-        def compute_values(rho,theta):
-            x = int(np.cos(theta)*rho)
-            y = int(np.sin(theta)*rho)
-            return (x, y)
+def compute_values(rho,theta):
+    x = int(np.cos(theta)*rho)
+    y = int(np.sin(theta)*rho)
+    return (x, y)
 
-        result = display_trackbar_window(
-            'test',
-            draw_image,
-            compute_values,
-            rho   = param(40,10,scale(10)),
-            theta = param(90,0,lambda x: (x/90.*360.)*np.pi/180.))
-
+result = display_trackbar_window(
+    'test',
+    draw_image,
+    compute_values,
+    rho   = param(40,10,scale(10)),
+    theta = param(90,0,lambda x: (x/90.*360.)*np.pi/180.))
