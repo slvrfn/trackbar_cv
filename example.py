a37a39c3c3cf52274fbc49d4e76715d6a6e5ab94
Cameron White <cameronwhite08@gmail.com>
2020-01-08 17:28:55 -0500

    basic reformatting

--- example.py
+++ example.py
@@ -3,26 +3,27 @@ from sys import exit
 
 from trackbar import *
 
+img = np.zeros((480, 640, 3))
+midpoint = tuple(np.roll(np.array(img.shape[0:2]) / 2, 1).astype(dtype=np.int))
 
-img = np.zeros((480,640,3))
-midpoint = tuple(np.roll(np.array(img.shape[0:2])/2,1).astype(dtype=np.int))
 
 def draw_image(point):
     new_image = np.copy(img)
-    (x,y) = point
-    cv2.line(new_image,(midpoint[0]+x,midpoint[1]+y),midpoint,[255,255,255])
-    return new_image.astype(dtype= np.uint8 )
+    (x, y) = point
+    cv2.line(new_image, (midpoint[0] + x, midpoint[1] + y), midpoint, [255, 255, 255])
+    return new_image.astype(dtype=np.uint8)
 
 
-def compute_values(rho,theta):
-    x = int(np.cos(theta)*rho)
-    y = int(np.sin(theta)*rho)
+def compute_values(rho, theta):
+    x = int(np.cos(theta) * rho)
+    y = int(np.sin(theta) * rho)
     return x, y
+
 
 result = display_trackbar_window(
     'test',
     draw_image,
     compute_values,
-    rho   = param(40,10,scale(10)),
-    theta = param(90,0,lambda x: (x/90.*360.)*np.pi/180.))
+    rho=param(40, 10, scale(10)),
+    theta=param(90, 0, lambda x: (x / 90. * 360.) * np.pi / 180.))
 exit()
--- trackbar.py
+++ trackbar.py
@@ -1,39 +1,49 @@
-
 import cv2 as cv2
 import copy
 
-# Some helper functions.
-def scale(factor): return lambda x: x*factor
-def identity(x): return x
-def param(count,initial_value = 0,adjust = identity ):
-    return (lambda **kwargs: kwargs)(count=count,initial_value=initial_value,adjust=adjust)
 
-#Display the trackbar window.
-def display_trackbar_window(window_name,draw_method,compute_values,**kwargs):
+# Some helper functions.
+def scale(factor): return lambda x: x * factor
+
+
+def identity(x): return x
+
+
+def param(count, initial_value=0, adjust=identity):
+    return (lambda **kwargs: kwargs)(count=count, initial_value=initial_value, adjust=adjust)
+
+
+# Display the trackbar window.
+def display_trackbar_window(window_name, draw_method, compute_values, **kwargs):
     args = {}
     last_args = {}
     adjust_fns = {}
     trackbar_window_name = window_name + ' - trackbar'
-    def nothing(*a,**k): pass
-    def to_tuple(count,initial_value,adjust): return count,initial_value,adjust
-    cv2.namedWindow(window_name,cv2.WINDOW_NORMAL|cv2.WINDOW_KEEPRATIO|cv2.WINDOW_GUI_EXPANDED)
-    cv2.namedWindow(trackbar_window_name,cv2.WINDOW_NORMAL|cv2.WINDOW_KEEPRATIO|cv2.WINDOW_GUI_EXPANDED)
-    for (key,value) in kwargs.items():
-        count,initial_value,adjust = to_tuple(**value)
+
+    def nothing(*a, **k):
+        pass
+
+    def to_tuple(count, initial_value, adjust):
+        return count, initial_value, adjust
+
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
+    cv2.namedWindow(trackbar_window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
+    for (key, value) in kwargs.items():
+        count, initial_value, adjust = to_tuple(**value)
         adjust_fns[key] = adjust
-        cv2.createTrackbar(key, trackbar_window_name,initial_value,count, nothing)
+        cv2.createTrackbar(key, trackbar_window_name, initial_value, count, nothing)
     while 1:
         k = cv2.waitKey(1) & 0xFF
-        if k==27:
+        if k == 27:
             break
-        for key,value in kwargs.items():
-            track_bar_pos = cv2.getTrackbarPos(key,trackbar_window_name)
+        for key, value in kwargs.items():
+            track_bar_pos = cv2.getTrackbarPos(key, trackbar_window_name)
             args[key] = adjust_fns[key](track_bar_pos)
         if args == last_args: continue
         last_args = copy.deepcopy(args)
         result = compute_values(**args)
         img = draw_method(result)
-        cv2.imshow(window_name,img)
+        cv2.imshow(window_name, img)
     cv2.destroyWindow(window_name)
     cv2.destroyWindow(trackbar_window_name)
     return last_args
