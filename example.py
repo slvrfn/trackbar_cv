4ed92dabf207bf05a95266906677ef230f71560f
Cameron White <cameronwhite08@gmail.com>
2020-01-08 17:27:09 -0500

    gitignore basic formatting made the trackbar appear in a separate window

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 import numpy as np
- 
+from sys import exit
+
 from trackbar import *
 
- 
+
 img = np.zeros((480,640,3))
 midpoint = tuple(np.roll(np.array(img.shape[0:2])/2,1).astype(dtype=np.int))
 
@@ -12,10 +13,11 @@ def draw_image(point):
     cv2.line(new_image,(midpoint[0]+x,midpoint[1]+y),midpoint,[255,255,255])
     return new_image.astype(dtype= np.uint8 )
 
+
 def compute_values(rho,theta):
     x = int(np.cos(theta)*rho)
     y = int(np.sin(theta)*rho)
-    return (x, y)
+    return x, y
 
 result = display_trackbar_window(
     'test',
@@ -23,3 +25,4 @@ result = display_trackbar_window(
     compute_values,
     rho   = param(40,10,scale(10)),
     theta = param(90,0,lambda x: (x/90.*360.)*np.pi/180.))
+exit()
